df63393feeb9036b288bc2bf3381fdc8573c6863
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2019-06-23 21:14:52 +0100

    Using Local Storage in the Data Service

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Question } from '../../models/Question';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
+
 export class QuestionComponent implements OnInit {
   @Input('question') question: Question;
+  isEdit = false;
 
-  constructor() { }
+  constructor(public dataService: DataService) { }
 
   ngOnInit() {
+  }
+
+  showQuestion() {
+    this.question.hide = !this.question.hide;
+  }
+
+  removeQuestion(question) {
+    this.dataService.removeQuestion(question);
+  }
+
+  editQuestion(question) {
+    this.isEdit = true;
+    this.question = question;
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,30 +8,70 @@ export class DataService {
   questions: Question[];
 
   constructor() {
-    this.questions = [
-      {
-        questiontext: 'What is your name?',
-        questionanswer: 'My name is Nwachukwu Ejiofor.',
-        hide: true
-      },
-      {
-        questiontext: 'What is your favourite colour?',
-        questionanswer: 'My favourite colour is blue.',
-        hide: true
-      },
-      {
-        questiontext: 'What is your favourite programming language?',
-        questionanswer: 'My favourite programming language is Java.',
-        hide: true
-      }
-    ];
+    // this.questions = [
+    //   {
+    //     questiontext: 'What is your name?',
+    //     questionanswer: 'My name is Nwachukwu Ejiofor.',
+    //     hide: true
+    //   },
+    //   {
+    //     questiontext: 'What is your favourite colour?',
+    //     questionanswer: 'My favourite colour is blue.',
+    //     hide: true
+    //   },
+    //   {
+    //     questiontext: 'What is your favourite programming language?',
+    //     questionanswer: 'My favourite programming language is Java.',
+    //     hide: true
+    //   }
+    // ];
   }
 
+  // Get questions from local storage
   getQuestions() {
+    if (localStorage.getItem('questions') === null) {
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
+
     return this.questions;
   }
 
+  // Add questions to local storage
   addQuestion(question: Question) {
     this.questions.unshift(question);
+
+    // Init local var
+    let questions;
+
+    if (localStorage.getItem('questions') === null) {
+      questions = [];
+
+      // Push new question
+      questions.unshift(question);
+
+      // Set new array to local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    } else {
+      questions = JSON.parse(localStorage.getItem('questions'));
+
+      // Add new question
+      questions.unshift(question);
+
+      // Reset local storage to updated array
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }
+  }
+
+  removeQuestion(question: Question) {
+    for (let i = 0; i < this.questions.length; i++) {
+      if (question === this.questions[i]) {
+        this.questions.splice(i, 1);
+
+        // Reset local storage to updated array
+        localStorage.setItem('questions', JSON.stringify(this.questions));
+      }
+    }
   }
 }
